b5cd05fda7c6c27845f70c63d279884cf73a554e
TAEJUN <43903354+htj7425@users.noreply.github.com>
2023-09-03 19:46:20 +0900

    JunDaeRee_Dynamic_ScrollView
    ScrollView 내부 Label의 텍스트 길이에 따라 ScrollView의 height가 Dynamic하게 정해짐

---
+++ JungDaeRee/UIKit_scrollview_tutorial/UIKit_scrollview_tutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit_scrollview_tutorial
+//
+//  Created by Taejun Ha on 2023/09/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
